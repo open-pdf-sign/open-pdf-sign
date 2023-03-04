0ccead20bd302a57e39712b14c24a97962dd7c25
Thomas Schreiber <thomas.schreiber@gmx.at>
2023-03-04 17:56:05 +0100

    update README

--- src/test/java/org/openpdfsign/SignatureVerifierTest.java
+++ src/test/java/org/openpdfsign/SignatureVerifierTest.java
@@ -10,9 +10,7 @@ class SignatureVerifierTest {
     @Test
     public void testVerifyValidSignature() throws IOException, CertificateException {
         SignatureVerifier verifier = new SignatureVerifier();
-        //verifier.verifyPdfSignature(Paths.get("signed2.pdf"), Paths.get("src/test/resources/atrust_corporate07.crt"));
-        //verifier.verifyPdfSignature(Paths.get("signed3nbcom.pdf"));
-
+        //@TODO
     }
 
 }
